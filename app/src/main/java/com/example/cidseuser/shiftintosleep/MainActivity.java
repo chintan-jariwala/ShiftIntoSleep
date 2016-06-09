01f9c005e5cf9345f64ad4105e0e4bc6a8bf905d
narnix17 <sabrinaxie11@gmail.com>
2016-06-09 12:36:59 -0700

    Settings

--- app/src/main/java/com/example/cidseuser/shiftintosleep/MainActivity.java
+++ app/src/main/java/com/example/cidseuser/shiftintosleep/MainActivity.java
@@ -1,9 +1,19 @@
 package com.example.cidseuser.shiftintosleep;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.CompoundButton;
+import android.widget.Spinner;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private ToggleButton vibrationToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,5 +21,35 @@ public class MainActivity extends AppCompatActivity {
 
         // Set the user interface layout for the front page
         setContentView(R.layout.activity_main);
+        vibrationToggle = (ToggleButton)findViewById(R.id.vib_toggle_button);
+
+        addToggleButtonListener();
+
+        Spinner spinner = (Spinner) findViewById(R.id.spin_ner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.language_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
     }
-}
+
+    private void addToggleButtonListener() {
+
+        vibrationToggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton toggleButton, boolean isChecked) {
+
+                SharedPreferences settings = getSharedPreferences("settings", 0);
+                SharedPreferences.Editor editor = settings.edit();
+                editor.putBoolean("vibrateon", isChecked);
+
+                editor.commit();
+            }
+
+        });
+
+    }
+
+
+        }
+
+
