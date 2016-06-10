903eccda1fd2b3cea9da64a4f9c81ef2ed5e53f0
mlaller <1856home>
2016-06-10 12:53:55 -0700

    Add Tips page

--- app/src/main/java/com/example/cidseuser/shiftintosleep/TipsFragment.java
+++ app/src/main/java/com/example/cidseuser/shiftintosleep/TipsFragment.java
@@ -5,13 +5,27 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.support.v4.app.Fragment;
+import android.text.Html;
+import android.text.Spanned;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
+
 
 /**
  * Created by cidseuser on 6/10/2016.
  */
 public class TipsFragment extends Fragment {
+    TextView HyperLink;
+    Spanned Text;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.tips, container, false);
-    }
+
+    HyperLink = (TextView) findViewById(R.id.tips);
+
+    Text = Html.fromHtml("Sleepwalking <br />" +
+         "<a href='http://www.sleepeducation.org/sleep-disorders-by-category/parasomnias/sleepwalking/overview-facts//'</a>");
+
+    HyperLink.setMovementMethod(LinkMovementMethod.getInstance());
+    HyperLink.setText(Text);}
 }
