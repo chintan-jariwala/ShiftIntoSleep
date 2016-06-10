8f4dfc3f8fc951f4d752065154b2c22c7ccc71e3
mlaller <1856home>
2016-06-10 10:00:17 -0700

    Final Master Layout and Function

--- app/src/main/java/com/example/cidseuser/shiftintosleep/SetupFragment.java
+++ app/src/main/java/com/example/cidseuser/shiftintosleep/SetupFragment.java
@@ -1,17 +1,17 @@
 package com.example.cidseuser.shiftintosleep;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.support.v4.app.Fragment;
 
 /**
- * Created by mlall on 6/9/2016.
+ * Created by cidseuser on 6/10/2016.
  */
-public class TabFragment extends Fragment {
+public class SetupFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.home, container, false);
+        return inflater.inflate(R.layout.setup, container, false);
     }
 }
--- app/src/main/java/com/example/cidseuser/shiftintosleep/ViewPagerAdapter.java
+++ app/src/main/java/com/example/cidseuser/shiftintosleep/ViewPagerAdapter.java
@@ -3,6 +3,8 @@ package com.example.cidseuser.shiftintosleep;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
+import android.util.Log;
+import android.widget.Toast;
 
 /**
  * Created by mlall on 6/9/2016.
@@ -14,7 +16,19 @@ public class ViewPagerAdapter extends FragmentStatePagerAdapter {
 
     @Override
     public Fragment getItem(int position) {
-        return new TabFragment(position);
+        Log.i("TESTing", "Position " + position);
+        if (position == 0) {
+            return new TabFragment();
+        } else if (position == 1) {
+            return new SetupFragment();
+        } else if (position == 2) {
+            return new AlarmsFragment();
+        } else if (position == 3) {
+            return new StatsFragment();
+        } else if (position == 4) {
+            return new TipsFragment();
+        }
+        return new TabFragment();
 
     }
 
