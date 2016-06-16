c8eaccc050c674a738db65f8c827bfce3fdcf6f0
mlaller <1856home>
2016-06-16 13:02:30 -0700

    Merged all tabs and tested

--- app/src/main/java/com/example/cidseuser/shiftintosleep/SettingsFragment.java
+++ app/src/main/java/com/example/cidseuser/shiftintosleep/SettingsFragment.java
@@ -12,6 +12,6 @@ import android.support.v4.app.Fragment;
 public class SettingsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.settings, container, false);
+        return inflater.inflate(R.layout.activity_settings, container, false);
     }
 }
--- app/src/main/java/com/example/cidseuser/shiftintosleep/StatsFragment.java
+++ app/src/main/java/com/example/cidseuser/shiftintosleep/StatsFragment.java
@@ -12,6 +12,6 @@ import android.support.v4.app.Fragment;
 public class StatsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.stats, container, false);
+        return inflater.inflate(R.layout.activity_personal_data, container, false);
     }
 }
