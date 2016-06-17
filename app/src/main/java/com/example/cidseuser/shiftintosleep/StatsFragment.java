624d3d233f343b7a50eec80e04d998a6c41c0992
mlaller <mlaller6@yahoo.com>
2016-06-17 09:38:17 -0700

    fixed graph

--- app/src/main/java/com/example/cidseuser/shiftintosleep/StatsFragment.java
+++ app/src/main/java/com/example/cidseuser/shiftintosleep/StatsFragment.java
@@ -1,10 +1,13 @@
 package com.example.cidseuser.shiftintosleep;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.widget.ButtonBarLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.support.v4.app.Fragment;
+import android.widget.Button;
 
 /**
  * Created by cidseuser on 6/10/2016.
@@ -12,6 +15,36 @@ import android.support.v4.app.Fragment;
 public class StatsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_personal_data, container, false);
+
+        View view = inflater.inflate(R.layout.activity_personal_data, container, false);
+
+        Button btn = (Button)view.findViewById(R.id.editButtonSchedule);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                        Intent intent = new Intent(getActivity(), CalenderActivity.class);
+                        startActivity(intent);
+            }
+        });
+
+        Button graphBtn = (Button)view.findViewById(R.id.Graphbtn);
+        graphBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), Graph.class);
+                startActivity(intent);
+            }
+        });
+
+        return view;
+    }
+
+    public void editButtonClicked(View view) {
+
+//        Intent intent = new Intent(this, CalenderActivity.class);
+//        startActivity(intent);
+
+        Intent intent = new Intent(getActivity(), Graph.class);
+        startActivity(intent);
     }
 }
